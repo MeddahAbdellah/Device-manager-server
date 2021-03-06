fbbda025afae7a20b7fc98392de86a894b97211e
Abdallah Meddah <meddah.abdellah.spcx@gmail.com>
2021-03-07 00:02:50 +0100

    fix(streaming) Wip

--- app/src/controllers.js
+++ app/src/controllers.js
@@ -58,10 +58,10 @@ export const createStreamingSessionController = (req, res ,io) => {
   const sessionId = getShaFromText(`${req.body.deviceName}${config.streamingSessionSecret}${(new Date()).toString()}`)
   io.of(`/${req.body.deviceName}`).emit("sessionInit", { sessionId });
   io.of(`/${sessionId}`).on('connection',(socket) =>{
-    socket.on('signal', (data) => console.log('/${sessionId}', data));
+    socket.on('sessionSignal', (data) => socket.broadcast.emit('sessionSignal', data));
   });
   io.of(`/test1`).on('connection',(socket) =>{
-    socket.on('signal', (data) => console.log('/${sessionId}', data));
+    socket.on('sessionSignal', (data) => socket.broadcast.emit('sessionSignal', data));
   });
   res.send({ sessionId });
 }
