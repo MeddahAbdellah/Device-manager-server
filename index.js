8118496aa123045d7a44bbd47f5f4d26ef61905b
Abdallah Meddah <meddah.abdellah.spcx@gmail.com>
2021-03-06 01:44:13 +0100

    fix(sha) fixed sha

--- index.js
+++ index.js
@@ -45,9 +45,10 @@ app.use(function(req, res, next) {
 });
 app.get('/', (req, res) => res.send('Server is up!'))
 app.post('/login', (req, res) => {
+
   con.query("SELECT * FROM users WHERE email=? AND password_hash=? ", [
     req.body.email,
-    (new jssha("SHA-256", "TEXT")).update(req.body.password).getHash("HEX"),
+    getShaFromText(req.body.password),
   ], (error, result) => {
     if (error) {
       console.error(error);
@@ -73,7 +74,7 @@ app.post('/register', (req,res) => {
   sqlParams = {
     name: req.body.name,
     email: req.body.email,
-    password_hash: (new jssha("SHA-256", "TEXT")).update(req.body.password).getHash("HEX"),
+    password_hash: getShaFromText(req.body.password),
   };
   con.query("INSERT INTO users SET ?", sqlParams, (error, result) => {
     if (error) {
@@ -86,5 +87,11 @@ app.post('/register', (req,res) => {
     }
   })
 })
+
+function getShaFromText(text) {
+  const sha = new jssha("SHA-256", "TEXT");
+  sha.update(text);
+  return sha.getHash('HEX');
+}
 
 app.listen(HTTP_PORT);
