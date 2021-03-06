9325e9c984602ccdff95b257c79c523701c1dd70
Abdallah Meddah <meddah.abdellah.spcx@gmail.com>
2021-03-06 21:59:28 +0100

    feat(socketIo): :sparkles: added socketIo

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import { VerifyToken } from '$/app/src/helpers.js';
 import { loginController, registerController } from '$/app/src/controllers.js';
 import https from 'https';
 import fs from 'fs';
+import { Server as SocketIoServer } from "socket.io";
 
 const app = express();
 const HTTP_PORT = 443;
-
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -19,22 +19,11 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json())
 //headers
-app.use(function(req, res, next) {
-
-  // Website you wish to allow to connect
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware()
   next();
 });
 app.get('/', (req, res) => res.send('Server is up!'))
@@ -42,11 +31,30 @@ app.post('/login', loginController);
 app.post('/register', registerController);
 app.get('/me', VerifyToken, (req, res) =>  res.status(200).send('able to access'));
 
-https.createServer({
+const httpsServer = https.createServer({
   key: fs.readFileSync('./key.pem'),
   cert: fs.readFileSync('./cert.pem'),
   passphrase: 'deviceManager',
 }, app)
-.listen(HTTP_PORT, () => {
+
+const io = new SocketIoServer(httpsServer, { cors: { origin: "*", methods: ["GET", "POST"] } });
+
+io.on("connection", (socket) => {
+});
+
+io.of('/test1').on("connection", (socket) => {
+  socket.on('message', (data) => {
+    console.log('from test1 ', data);
+  });
+});
+
+io.of('/test2').on("connection", (socket) => {
+  socket.on('message', (data) => {
+    console.log('from test2 ', data);
+    io.of('/test1').emit('fromTest2', data);
+  });
+});
+
+httpsServer.listen(HTTP_PORT, () => {
   console.log(`Https server running on port ${HTTP_PORT}`);
 });
