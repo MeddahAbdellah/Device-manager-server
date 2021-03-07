b4554cb110a14f344b6235566e2d816b52cb00bc
Abdallah Meddah <meddah.abdellah.spcx@gmail.com>
2021-03-07 19:33:22 +0100

    fix(devices): fixed typo

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.post('/login', loginController);
 app.post('/register', registerController);
 app.post('/createStreamingSession', VerifyToken, (req, res) => createStreamingSessionController(req, res, io));
 app.post('/addDevice', VerifyToken, (req, res) => addDeviceController(req, res));
-app.get('/getDeivces', VerifyToken, userOwnsDevice, (req, res) => getDevicesController(req, res, io));
+app.get('/getDevices', VerifyToken, userOwnsDevice, (req, res) => getDevicesController(req, res, io));
 
 app.get('/me', VerifyToken, (req, res) =>  res.status(200).send({ auth: true }));
 
