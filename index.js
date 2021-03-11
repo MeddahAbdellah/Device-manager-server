5f17375df166d5da4b0da7430dfe2d9b2492f9c8
Abdallah Meddah <meddah.abdellah.spcx@gmail.com>
2021-03-11 23:35:11 +0100

    feat(fileSharing): added file sharing backend

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import {
   createStreamingSessionController,
   addDeviceController,
   getDevicesController,
+  getFileSharingSessionForDevice,
 } from '$/app/src/controllers.js';
 import https from 'https';
 import fs from 'fs';
@@ -44,6 +45,26 @@ io.of(`/devices`).on('connection', (socket) => {
   });
 });
 
+io.of(`/files`).on('connection', (socket) => {
+
+  socket.on('connectToSession', (data) => {
+    console.log('FILES: connectToSession', data);
+    socket.join(data.sessionId);
+  });
+
+  socket.on('sendSignal', (data) => {
+    console.log('FILES: sendSignal', data);
+    socket.to(data.sessionId).emit('listenSignal', data)
+  });
+});
+
+io.of(`/fileDevices`).on('connection', (socket) => {
+  socket.on('addToConnectedDevices', (data) => {
+    console.log('FILES: addToConnectedDevices', data);
+    socket.join(data.deviceName);
+  });
+});
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 // parse application/x-www-form-urlencoded
@@ -63,6 +84,7 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => res.send('Server is up!'))
 app.post('/login', loginController);
 app.post('/register', registerController);
+app.post('/getFileSharingSessionForDevice', VerifyToken, userOwnsDevice, (req, res) => getFileSharingSessionForDevice(req, res, io));
 app.post('/createStreamingSession', VerifyToken, userOwnsDevice, (req, res) => createStreamingSessionController(req, res, io));
 app.post('/addDevice', VerifyToken, (req, res) => addDeviceController(req, res));
 app.get('/getDevices', VerifyToken, (req, res) => getDevicesController(req, res, io));
